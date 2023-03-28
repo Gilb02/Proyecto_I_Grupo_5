17ec0d6126037b947f2ad3b0eff86157268da84b
Pil <gilberth.vr@gmail.com>
2023-03-27 21:41:07 -0600

    Filtro

--- Filtro.py
+++ Filtro.py
@@ -4,23 +4,7 @@ from gi.repository import Gst, GObject
 
 Gst.init(None)
 
-pipeline = Gst.Pipeline()
-src = Gst.ElementFactory.make("v4l2src")
-videoconvert1 = Gst.ElementFactory.make("videoconvert")
-edge = Gst.ElementFactory.make("edgetv")
-videoconvert2 = Gst.ElementFactory.make("videoconvert")
-ximagesink = Gst.ElementFactory.make("ximagesink")
-
-pipeline.add(src)
-pipeline.add(videoconvert1)
-pipeline.add(edge)
-pipeline.add(videoconvert2)
-pipeline.add(ximagesink)
-
-src.link(videoconvert1)
-videoconvert1.link(edge)
-edge.link(videoconvert2)
-videoconvert2.link(ximagesink)
+pipeline = Gst.parse_launch("v4l2src ! videoconvert ! edgetv ! videoconvert ! ximagesink")
 
 pipeline.set_state(Gst.State.PLAYING)
 
@@ -28,3 +12,4 @@ bus = pipeline.get_bus()
 msg = bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.EOS)
 
 pipeline.set_state(Gst.State.NULL)
+
